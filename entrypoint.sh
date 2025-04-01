361b590691515bc981f0c3894a4c752ca78d390b
Tianyu Liu <tialiu@nvidia.com>
2025-04-01 04:15:55 +0000

    Update

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y make gcc git
+pip install linetimer pydantic pydantic-settings
+/bin/bash
--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+workdir=/home/rapids
+
 docker run --name tialiu-polars-benchmark-1 --rm \
---workdir=/home/rapids \
+--workdir=$workdir \
 --gpus all --pull always -it --privileged=true --cap-add=SYS_ADMIN --user=root \
---mount type=bind,src=/nvme/1/tialiu/polars/polars-benchmark,dst=/home/rapids/polars-benchmark \
---mount type=bind,src=$(pwd),dst=/home/rapids/local \
-rapidsai/base:25.04a-cuda12.8-py3.12 \
-bash
-# /bin/bash -c "apt update && apt install -y make gcc git && pip install linetimer pydantic pydantic-settings"
+--mount type=bind,src=/nvme/1/tialiu/polars/polars-benchmark,dst=$workdir/polars-benchmark \
+--mount type=bind,src=.,dst=$workdir \
+--hostname=custom-polars-benchmark \
+--entrypoint="$workdir/entrypoint.sh" \
+rapidsai/base:25.04a-cuda12.8-py3.12
