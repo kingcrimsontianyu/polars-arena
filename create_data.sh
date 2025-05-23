d0ccc6dbbfbfa92e66fd05e26c7430b42fb92d5e
Tianyu Liu <tialiu@nvidia.com>
2025-05-23 14:21:36 +0000

    Update

--- create_data.sh
+++ create_data.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# If there is data generation error, remove the .venv directory in $polar_bench_dir, and rerun.
+
 polar_bench_dir=/home/rapids/polars-benchmark
 
 cd $polar_bench_dir/tpch-dbgen
 make -j
 cd $polar_bench_dir
+
 # SCALE_FACTOR=100.0 make data/tables/
-SCALE_FACTOR=1.0 make data/tables/
+
+# SCALE_FACTOR=1.0 make tables-pyarrow
+SCALE_FACTOR=100.0 make tables-pyarrow
--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# original
+# polars_benchmark_dir=polars-benchmark
+# fireduck
+polars_benchmark_dir=polars-tpch
+
 workdir=/home/rapids
 
 docker run --name tialiu-polars-benchmark --rm \
 --workdir=$workdir \
 --gpus all --pull always -it --privileged=true --cap-add=SYS_ADMIN --user=root \
---mount type=bind,src=$(pwd)/../polars-benchmark,dst=$workdir/polars-benchmark \
+--mount type=bind,src=$(pwd)/../$polars_benchmark_dir,dst=$workdir/polars-benchmark \
 --mount type=bind,src=.,dst=$workdir \
 --hostname=custom-polars-benchmark \
 --entrypoint="$workdir/entrypoint.sh" \
-rapidsai/base:25.06a-cuda12.8-py3.13
+rapidsai/base:25.04-cuda12.8-py3.11
